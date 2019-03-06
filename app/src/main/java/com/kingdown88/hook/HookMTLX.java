572826d1c99cbfc0bb7fbe0a1b5141119924517f
kingdown88 <qc_f741aed4084e49c3b40354525e0bb7b2@git.code.tencent.com>
2019-03-06 14:04:52 +0800

    ctrip JsonUtils

--- app/src/main/java/com/kingdown88/hook/HookMTLX.java
+++ app/src/main/java/com/kingdown88/hook/HookMTLX.java
@@ -13,27 +13,18 @@ import static com.kingdown88.hook.HookLog.log;
 
 public class HookMTLX implements IXposedHookLoadPackage {
 
+  //adb shell logcat -s zjdroid-shell-com.meituan.tower
+  //adb shell logcat -s zjdroid-apimonitor-com.meituan.tower
+  //private static final String FILTER_PKGNAME = "com.meituan.tower";
+
+  //adb shell logcat -s zjdroid-shell-ctrip.android.view
+  //adb shell logcat -s zjdroid-apimonitor-ctrip.android.view
+  private static final String FILTER_PKGNAME = "ctrip.android.view";
+
   @Override public void handleLoadPackage(final LoadPackageParam loadPackageParam) {
     String pkgname = loadPackageParam.packageName;
     log("pkgname-->" + pkgname);
     if (FILTER_PKGNAME.equals(pkgname) && loadPackageParam.isFirstApplication) {
-      //XposedHelpers.findAndHookMethod(ClassLoader.class, "loadClass", String.class,
-      //    new XC_MethodHook() {
-      //      @Override protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-      //        if (param.hasThrowable()) {
-      //          return;
-      //        }
-      //        try {
-      //          Class<?> cls = (Class<?>) param.getResult();
-      //          String name = cls.getName();
-      //          if (!TextUtils.isEmpty(name) && (name.contains("sankuai") || name.contains(
-      //              "meituan"))) {
-      //            log("name-->" + name);
-      //          }
-      //        } catch (Exception e) {
-      //        }
-      //      }
-      //    });
 
       XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class,
           new XC_MethodHook() {
@@ -41,68 +32,50 @@ public class HookMTLX implements IXposedHookLoadPackage {
               Context context = (Context) param.args[0];
               ClassLoader classLoader = context.getClassLoader();
 
+              log("======================================================================");
               Class<?> c;
 
-              //c = classLoader.loadClass("com.sankuai.meituan.retrofit2.Retrofit");
-              //printClassMethod(c);
-              //
-              //XposedHelpers.findAndHookMethod(c, "requestBodyConverter",
-              //    java.lang.reflect.Type.class, "[Ljava.lang.annotation.Annotation;",
-              //    "[Ljava.lang.annotation.Annotation;", new XC_MethodHook() {
-              //      @Override protected void beforeHookedMethod(MethodHookParam param)
-              //          throws Throwable {
-              //        super.beforeHookedMethod(param);
-              //      }
-              //
-              //      @Override protected void afterHookedMethod(MethodHookParam param)
-              //          throws Throwable {
-              //        super.afterHookedMethod(param);
-              //        log("afterHookedMethod args  =" + param.args[0]);
-              //        log("afterHookedMethod result=" + param.getResult());
-              //      }
-              //    });
-
-              log("======================================================================");
-
-              c = classLoader.loadClass("com.meituan.retrofit2.androidadapter.a");
-              //printClassMethod(c);
-
-              //final Class<?> ArgClass= XposedHelpers.findClass("aqcxbom.xposedhooktarget.ArgClass", classLoader);
-              //final Class<?> ArrayList= XposedHelpers.findClass("java.util.ArrayList", classLoader);
-
-              final Class<?> CallClass =
-                  XposedHelpers.findClass("com.sankuai.meituan.retrofit2.Call", classLoader);
-              final Class<?> ResponseClass =
-                  XposedHelpers.findClass("com.sankuai.meituan.retrofit2.Response", classLoader);
-
-              final Class<?> Map = XposedHelpers.findClass("java.util.Map", classLoader);
+              c = classLoader.loadClass("ctrip.foundation.util.JsonUtils");
+              printClassMethod(c);
 
               //"[[Ljava.lang.String;", //参数1
-              //    Map, //参数2
-              //    Map, //参数3
-              //    Map, //参数4
-              //    ArrayList, //参数5
-              //    ArrayList, //参数6
-              //    ArgClass, //参数7
+              //final Class<?> ArgClass= XposedHelpers.findClass("aqcxbom.xposedhooktarget.ArgClass", classLoader);
+              //final Class<?> ArrayList= XposedHelpers.findClass("java.util.ArrayList", classLoader);
+              //final Class<?> Map = XposedHelpers.findClass("java.util.Map", classLoader);
+              final Class<?> cClass = XposedHelpers.findClass("java.lang.Class", classLoader);
 
-              final Class<?> chain =
-                  XposedHelpers.findClass("com.sankuai.meituan.retrofit2.Interceptor.Chain",
-                      classLoader);
+              final Class<?> cObject = XposedHelpers.findClass("java.lang.Object", classLoader);
 
-              XposedHelpers.findAndHookMethod(c, "onResponse", CallClass, ResponseClass,
+              XposedHelpers.findAndHookMethod(c, "parse", String.class, cClass,
                   new XC_MethodHook() {
                     @Override protected void beforeHookedMethod(MethodHookParam param)
                         throws Throwable {
                       super.beforeHookedMethod(param);
-                      log("beforeHookedMethod args  =" + param.args[0]);
+                      log("parse ``````````````````````````````````````````");
+                      log("b  =" + param.args[0]);
+                      log("b  =" + param.args[1]);
                     }
 
                     @Override protected void afterHookedMethod(MethodHookParam param)
                         throws Throwable {
                       super.afterHookedMethod(param);
-                      log("afterHookedMethod result=" + param.getResult());
+                      log("a  =" + param.getResult());
                     }
                   });
+
+              XposedHelpers.findAndHookMethod(c, "toJson", cObject, new XC_MethodHook() {
+                @Override protected void beforeHookedMethod(MethodHookParam param)
+                    throws Throwable {
+                  super.beforeHookedMethod(param);
+                  log("toJson ``````````````````````````````````````````");
+                  log("b  =" + param.args[0]);
+                }
+
+                @Override protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                  super.afterHookedMethod(param);
+                  log("a  =" + param.getResult());
+                }
+              });
               log("///////////////======================================================================");
             }
           });
@@ -115,6 +88,4 @@ public class HookMTLX implements IXposedHookLoadPackage {
       log("methodName=====> " + getMethodSign(method));
     }
   }
-
-  private static final String FILTER_PKGNAME = "com.meituan.tower";
 }
