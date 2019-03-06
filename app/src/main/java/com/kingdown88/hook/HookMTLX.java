56aeeb2f457312bf50e73fdbbca0a01c0350a4f2
kingdown88 <qc_f741aed4084e49c3b40354525e0bb7b2@git.code.tencent.com>
2019-03-06 14:32:50 +0800

    ctrip CtripHTTPClientV2

--- app/src/main/java/com/kingdown88/hook/HookMTLX.java
+++ app/src/main/java/com/kingdown88/hook/HookMTLX.java
@@ -36,24 +36,63 @@ public class HookMTLX implements IXposedHookLoadPackage {
               Class<?> c;
 
               c = classLoader.loadClass("ctrip.foundation.util.JsonUtils");
-              printClassMethod(c);
+              c = classLoader.loadClass("ctrip.foundation.util.CtripURLUtil");
+              c = classLoader.loadClass("ctrip.android.http.CtripHTTPClientV2");
+              //printClassMethod(c);
 
               //"[[Ljava.lang.String;", //参数1
               //final Class<?> ArgClass= XposedHelpers.findClass("aqcxbom.xposedhooktarget.ArgClass", classLoader);
               //final Class<?> ArrayList= XposedHelpers.findClass("java.util.ArrayList", classLoader);
-              //final Class<?> Map = XposedHelpers.findClass("java.util.Map", classLoader);
+              final Class<?> Map = XposedHelpers.findClass("java.util.Map", classLoader);
               final Class<?> cClass = XposedHelpers.findClass("java.lang.Class", classLoader);
-
               final Class<?> cObject = XposedHelpers.findClass("java.lang.Object", classLoader);
+              final Class<?> CtripHTTPCallbackV2 =
+                  XposedHelpers.findClass("ctrip.android.http.CtripHTTPCallbackV2", classLoader);
 
-              XposedHelpers.findAndHookMethod(c, "parse", String.class, cClass,
+              //XposedHelpers.findAndHookMethod(c, "parse", String.class, cClass,
+              //    new XC_MethodHook() {
+              //      @Override protected void beforeHookedMethod(MethodHookParam param)
+              //          throws Throwable {
+              //        super.beforeHookedMethod(param);
+              //        log("``````````````````````````````````````````");
+              //        log("b  =" + param.args[0]);
+              //        log("b  =" + param.args[1]);
+              //      }
+              //
+              //      @Override protected void afterHookedMethod(MethodHookParam param)
+              //          throws Throwable {
+              //        super.afterHookedMethod(param);
+              //        log("a  =" + param.getResult());
+              //      }
+              //    });
+              //
+              //XposedHelpers.findAndHookMethod(c, "toJson", cObject, new XC_MethodHook() {
+              //  @Override protected void beforeHookedMethod(MethodHookParam param)
+              //      throws Throwable {
+              //    super.beforeHookedMethod(param);
+              //    log("``````````````````````````````````````````");
+              //    log("b  =" + param.args[0]);
+              //  }
+              //
+              //  @Override protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+              //    super.afterHookedMethod(param);
+              //    log("a  =" + param.getResult());
+              //  }
+              //});
+
+              XposedHelpers.findAndHookMethod(c, "asyncPostWithTimeout", String.class, String.class,
+                  CtripHTTPCallbackV2, int.class, Map, boolean.class, boolean.class,
                   new XC_MethodHook() {
                     @Override protected void beforeHookedMethod(MethodHookParam param)
                         throws Throwable {
                       super.beforeHookedMethod(param);
-                      log("parse ``````````````````````````````````````````");
+                      log("``````````````````````````````````````````");
                       log("b  =" + param.args[0]);
                       log("b  =" + param.args[1]);
+                      log("b  =" + param.args[3]);
+                      log("b  =" + param.args[4]);
+                      log("b  =" + param.args[5]);
+                      log("b  =" + param.args[6]);
                     }
 
                     @Override protected void afterHookedMethod(MethodHookParam param)
@@ -62,20 +101,6 @@ public class HookMTLX implements IXposedHookLoadPackage {
                       log("a  =" + param.getResult());
                     }
                   });
-
-              XposedHelpers.findAndHookMethod(c, "toJson", cObject, new XC_MethodHook() {
-                @Override protected void beforeHookedMethod(MethodHookParam param)
-                    throws Throwable {
-                  super.beforeHookedMethod(param);
-                  log("toJson ``````````````````````````````````````````");
-                  log("b  =" + param.args[0]);
-                }
-
-                @Override protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                  super.afterHookedMethod(param);
-                  log("a  =" + param.getResult());
-                }
-              });
               log("///////////////======================================================================");
             }
           });
